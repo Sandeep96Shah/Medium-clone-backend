dcfcc2f7924ba9f9ec4ab0d0628b8115b565b6dc
Sandeep Kumar Shah <sandeep2016shah@gmail.com>
2023-01-01 19:15:00 +0530

    corrected the cors issue

--- backend/index.js
+++ backend/index.js
@@ -11,8 +11,8 @@ app.use(express.urlencoded({extended: true}));
 app.use(express.json({limit: '50mb'}));
 app.use(cors());
 app.use(function(req,res,next){
-    res.header("Access-Control-Allow-Origin","*");
-    res.header("Access-Control-Allow-Methods", "GET, POST, DELETE, OPTIONS");
+    res.setHeader("Access-Control-Allow-Origin","*");
+    res.header("Access-Control-Allow-Methods", "GET, POST");
     res.header(
         "Access-Control-Allow-Headers",
         "Origin, X-Requested:With, Content-Type, Accept"
